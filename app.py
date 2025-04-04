24f9b6b0b612beb469b8984f41aae8f37c2a9b2c
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-04-04 12:38:16 +0200

    Eliminar tamaño de fuente del encabezado en la sección de evaluación de leads para mantener la consistencia del estilo.

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ st.markdown("""
 # Header personalizado moderno
 st.markdown("""
 <div style='background-color:#262730; padding: 1rem; border-radius: 10px; margin-bottom: 2rem; text-align:center;'>
-    <h2  style='color:#4FC3F7; font-size: 1rem; margin-bottom: 0.5rem;'>Evalúa automáticamente la intención de compra de tus leads y clasifícalos como Frío, Tibio o Caliente.</h2>
+    <h3  style='color:#4FC3F7; margin-bottom: 0.5rem;'>Evalúa automáticamente la intención de compra de tus leads y clasifícalos como Frío, Tibio o Caliente.</h3>
 </div>
 """, unsafe_allow_html=True)
 
